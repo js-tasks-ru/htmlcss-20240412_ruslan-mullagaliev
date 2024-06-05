df170db3072f2b7c5c9ade30335cd563786cadbb
ruslannm <brak@mail.ru>
2024-06-05 22:21:18 +0300

    08-lection6: process

--- gulpfile.js
+++ gulpfile.js
@@ -53,6 +53,50 @@ gulp.task('format-index', function (done) {
   done();
 });
 
+// Сборка компонентов
+const componets = JSON.parse(
+  readFileSync('./data/component-page/data.json', 'utf8'),
+);
+
+componets.forEach((item) => {
+  gulp.task(`pre-${item.url}`, function () {
+    console.log('item', item);
+    const options = {
+      batch: ['./views/partials'],
+      helpers: {
+        partialStr: function (str) {
+          return new handlebars.Handlebars.SafeString('{{> ' + str + '}}');
+        },
+      },
+    };
+    return gulp
+      .src('./views/template/component-page.hbs')
+      .pipe(handlebars(item, options))
+      .pipe(rename(`${item.url}.hbs`))
+      .pipe(gulp.dest('./views/layout'));
+  });
+
+  gulp.task(item.url, function () {
+    const options = {
+      batch: ['./views/partials'],
+    };
+    return gulp
+      .src(`./views/layout/${item.url}.hbs`)
+      .pipe(handlebars(null, options))
+      .pipe(rename(`${item.url}.html`))
+      .pipe(gulp.dest('./dist'));
+  });
+
+  gulp.task(`format-${item.url}`, function (done) {
+    const settings = JSON.parse(readFileSync('./.prettierrc', 'utf8'));
+    settings['parser'] = 'html';
+    const sourceHTML = readFileSync(`./dist/${item.url}.html`, 'utf8');
+    const formattedHTML = prettier.format(sourceHTML, settings);
+    writeFileSync(`./${item.url}.html`, formattedHTML);
+    done();
+  });
+});
+
 gulp.task(
   'default',
   gulp.series('preview-histogram', 'preview-tabs', 'index', 'format-index'),
---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  syntax: 'postcss-scss',
+  map: { inline: true },
+  plugins: [
+    require('precss')({
+      lookup: { disable: true },
+      stage: false,
+      properties: {
+        // Чтобы работали CSS custom properties, а не заменялись на статическое значение.
+        disable: true,
+        preserve: true,
+      },
+      path: ['./'],
+    }),
+    require('postcss-import')({ path: ['./'] }),
+    // require('postcss-nested'),
+    // require('autoprefixer'),
+    // require('cssnano'),
+  ],
+};
