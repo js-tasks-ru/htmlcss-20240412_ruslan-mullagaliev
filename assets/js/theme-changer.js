ae1c4a8385084a7c6e7cdc5e2bc03937f48d191e
ruslannm <brak@mail.ru>
2024-07-15 20:39:29 +0300

    10-lection8-01-theme-changer: add changer to burger-menu

--- assets/js/theme-changer.js
+++ assets/js/theme-changer.js
@@ -1,7 +1,10 @@
 const changer = document.getElementById('theme-changer');
+const changerBurgerMenu = document.getElementById('theme-changer-burger-menu');
 const html = document.documentElement;
 
 changer.addEventListener('change', function () {
-  console.log('aaa');
+  html.toggleAttribute('data-theme-dark');
+});
+changerBurgerMenu.addEventListener('change', function () {
   html.toggleAttribute('data-theme-dark');
 });
